e4f3a9e81297b2937a18ecdee0094e183b3f5a47
Demir-Utku <utkdmr1@gmail.com>
2020-11-07 11:25:41 +0000

    Update the solution

--- Hafta01/SayiTahmin.cpp
+++ Hafta01/SayiTahmin.cpp
@@ -4,22 +4,13 @@
 * Değerler eşitse, ekrana, "tebrikler!" yazar
 * Değer farklıysa "Tekrar deneyin!" yazar
 */
+
 #include <iostream>
-<<<<<<< HEAD
-#include <string>
-=======
->>>>>>> b8ab5bb... İlk hafta odevi yapildi
 
 int main()
 {
-    // sayı tut
-<<<<<<< HEAD
-    int tut = 7;
-    int tahmin = 0;
-=======
     int sayi = 7;
     int tahmin;
->>>>>>> b8ab5bb... İlk hafta odevi yapildi
 
     // kullanıcıya bilgi ver -> bu program şu işe yarar...
     // kullanıcıdan 1 ile 10 arası sayı girmsini iste
@@ -27,8 +18,6 @@ int main()
     // değerler eşitse tebrik et --> if
     // değilse, tekrar deneyin yaz --> else
 
-<<<<<<< HEAD
-=======
     std::cout << "Bu program, 1 ile 10 arasında bir sayı tutar, kullanıcıdan tamsayı ister." << std::endl;
     std::cout << "Değerler eşitse, ekrana, 'tebrikler!' yazar" << std::endl;
     std::cout << "Değer farklıysa 'Tekrar deneyin!' yazar" << std::endl;
@@ -43,6 +32,5 @@ int main()
         std::cout << "Tekrar deneyin!" << std::endl;
     }
 
->>>>>>> b8ab5bb... İlk hafta odevi yapildi
     return 0;
 }
